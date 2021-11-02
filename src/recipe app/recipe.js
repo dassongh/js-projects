22f033fd1cc33bded60f25ab581d0f52425a415f
Oleksandr S <oleksandr.slv@gmail.com>
2021-11-02 18:34:42 +0200

    adds info about meal

--- src/recipe app/recipe.js
+++ src/recipe app/recipe.js
@@ -6,13 +6,44 @@ const refs = {
   categoriesBtns: document.querySelectorAll('[data-name]'),
   input: document.querySelector('#input'),
   searchBtn: document.querySelector('#search'),
-}
+};
 const fetchMeals = new FetchMeals;
 
-const renderMeal = ({ strMealThumb, strMeal }) => {
+const renderInfo = ({ strMeal, strMealThumb, strInstructions }) => {
+  const markup = `<div class="modal-overlay">
+                    <div class="modal">
+                      <div class="modal-header">
+                        <button type="button" class="modal-close"><i class="fas fa-times"></i></button>
+                        <h4>${strMeal}</h4>
+                        <img src="${strMealThumb}" alt="${strMeal}">
+                      </div>
+                      <div class="modal-body">
+                        <p>${strInstructions}</p>
+                      </div>
+                    </div>
+                  </div>`;
+  
+  document.body.insertAdjacentHTML('beforeend', markup);
+
+  const closeBtnRef = document.querySelector('.modal-close');
+  const modalRef = document.querySelector('.modal-overlay');
+
+  closeBtnRef.addEventListener('click', () => {
+    modalRef.classList.add('is-hidden');
+    
+  });
+};
+
+const toggleHeartBtn = () => {
+  const btnsHeart = refs.mealsSection.querySelectorAll('button');
+  btnsHeart.forEach(btn => btn.addEventListener('click', () => btn.classList.toggle('active')));
+};
+
+const renderMeal = (meal, random = false) => {
+  const { strMealThumb, strMeal } = meal;
   const markup = `<article class="meal">
                     <div class="meal-header">
-                      <h4>Random meal</h4>
+                      ${random ? '<h4>Random meal</h4>' : ''}
                       <img src="${strMealThumb}" alt="preview of a meal">
                     </div>
                     <div class="meal-body">
@@ -22,32 +53,17 @@ const renderMeal = ({ strMealThumb, strMeal }) => {
                   </article>`;
   
   refs.mealsSection.insertAdjacentHTML('afterbegin', markup);
+  toggleHeartBtn();
+  
+  const mealRef = refs.mealsSection.querySelector('.meal');
+  
+  mealRef.addEventListener('click', () => {
+    renderInfo(meal);
+  });
 };
-
-const renderMeals = meals => {
-  const markup = meals.map(({ strMealThumb, strMeal }) => {
-    return `<article class="meal">
-              <div class="meal-header">
-                <img src="${strMealThumb}" alt="preview of a meal">
-              </div>
-              <div class="meal-body">
-                <h4>${strMeal}</h4>
-                <button type="button"><i class="fas fa-heart"></i></button>
-              </div>
-            </article>`
-  }).join('');
-
-  refs.mealsSection.insertAdjacentHTML('beforeend', markup);
-}
-
-const toggleHeartBtn = () => {
-  const btnsHeart = refs.mealsSection.querySelectorAll('button');
-  btnsHeart.forEach(btn => btn.addEventListener('click', () => btn.classList.toggle('active')));
-}
 
 const loadRandomMeal = () => fetchMeals.byRandom().then(data => {
   renderMeal(data.meals[0], true);
-  toggleHeartBtn();
 });
 
 loadRandomMeal();
@@ -59,8 +75,7 @@ refs.categoriesBtns.forEach(btn => {
     refs.mealsSection.innerHTML = '';
     
     fetchMeals.byCategory(category).then(data => {
-      renderMeals(data.meals);
-      toggleHeartBtn();
+      data.meals.forEach(meal => renderMeal(meal));
     });
   })
 });
@@ -71,7 +86,6 @@ refs.searchBtn.addEventListener('click', () => {
   refs.mealsSection.innerHTML = '';
 
   fetchMeals.byName(name).then(data => {
-    renderMeals(data.meals);
-    toggleHeartBtn();
+    data.meals.forEach(meal => renderMeal(meal));
   }).catch(err => Notify.failure('Could not find anything'));
 });
