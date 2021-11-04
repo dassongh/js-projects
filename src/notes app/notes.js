a9b8c28427feee504ff9c42eed2a2afba55e665c
Oleksandr S <oleksandr.slv@gmail.com>
2021-11-04 18:19:03 +0200

    adds functionality to notes app

--- src/notes app/notes.js
+++ src/notes app/notes.js
@@ -1,20 +1,57 @@
-const refs = {
-  main: document.querySelector('.note-main'),
-  textarea: document.querySelector('textarea'),
-  editBtn: document.querySelector('[data-edit]'),
-}
+const addBtnRef = document.querySelector('.btn-add');
+const notesData = JSON.parse(localStorage.getItem('notes'));
 
-refs.editBtn.addEventListener('click', () => {
-  refs.textarea.classList.toggle('is-hidden');
-  refs.main.classList.toggle('is-hidden');
+if (notesData) notesData.forEach(note => addNote(note));
 
-  // refs.main.textContent = marked(refs.textarea.value);
+addBtnRef.addEventListener('click', () => {
+  addNote();
 });
 
-refs.textarea.addEventListener('input', (e) => {
-  const { value } = e.currentTarget
-  console.log(value)
-  // refs.main.innerHTML = marked(value);
-})
+function setToLS() {
+  const data = document.querySelectorAll('textarea');
 
-console.log(marked())
+  const notes = [];
+
+  data.forEach(note => notes.push(note.value));
+
+  localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+function addNote(data = '') {
+  const note = document.createElement('div');
+  note.classList.add('note');
+
+  const markup = `<div class="note-header">
+                    <button class="btn" data-edit><i class="fas fa-edit"></i></button>
+                    <button class="btn" data-delete><i class="fas fa-trash-alt"></i></button>
+                  </div>
+                  <div class="note-main ${data ? '' : 'is-hidden'}">
+                  </div>
+                  <textarea class="${data ? 'is-hidden' : ''}">${data}</textarea>`;
+  note.insertAdjacentHTML('afterbegin', markup);
+
+  const refs = {
+    main: note.querySelector('.note-main'),
+    textarea: note.querySelector('textarea'),
+    editBtn: note.querySelector('[data-edit]'),
+    deleteBtn: note.querySelector('[data-delete]'),
+  };
+
+  refs.main.textContent = refs.textarea.value;
+
+  refs.editBtn.addEventListener('click', () => {
+    refs.textarea.classList.toggle('is-hidden');
+    refs.main.classList.toggle('is-hidden');
+
+    refs.main.textContent = refs.textarea.value;
+  });
+
+  refs.deleteBtn.addEventListener('click', () => {
+    note.remove();
+    setToLS();
+  });
+
+  refs.textarea.addEventListener('input', setToLS);
+
+  document.body.appendChild(note);
+}
