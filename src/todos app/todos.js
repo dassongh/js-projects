5830b8007533c2f44a9bd474c6ce4e2a896fb1b5
Oleksandr S <oleksandr.slv@gmail.com>
2021-11-07 19:52:40 +0200

    adds ability to add new todo and to toggle todos

---
+++ src/todos app/todos.js
@@ -0,0 +1,133 @@
+const refs = {
+  form: document.getElementById('form'),
+  todosUl: document.querySelector('.todos'),
+  radioBtns: document.querySelectorAll('.radio'),
+};
+
+refs.radioBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const todoRefs = document.querySelectorAll('.todo');
+    const radioElement = e.target;
+    const allRef = document.getElementById('all');
+    const finishedRef = document.getElementById('finished');
+    const unfinishedRef = document.getElementById('unfinished');
+
+    if (finishedRef.checked) {
+      
+    }
+
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+    // if (finishedRef.checked) {
+    //   todoRefs.forEach(todo => {
+    //     if (!todo.classList.contains('checked')) {
+    //       todo.classList.add('is-hidden');
+    //       radioElement.parentElement.classList.add('radio-checked');
+    //       radioElement.parentElement.previousElementSibling.classList.remove('radio-checked');
+    //       radioElement.parentElement.nextElementSibling.classList.remove('radio-checked');
+    //     }
+    //   });
+    // }
+    
+    // if (unfinishedRef.checked) {
+    //   todoRefs.forEach(todo => {
+    //     if (todo.classList.contains('checked')) {
+    //       todo.classList.add('is-hidden');
+    //       radioElement.parentElement.classList.add('radio-checked');
+    //       radioElement.parentElement.previousElementSibling.classList.remove('radio-checked');
+    //       radioElement.parentElement.previousElementSibling.previousElementSibling.classList.remove('radio-checked');
+    //     }
+    //   });
+    // }
+
+    // todoRefs.forEach(todo => {
+    //   if (finishedRef.checked && !todo.classList.contains('checked')) {
+    //     todo.classList.add('is-hidden');
+    //     radioElement.parentElement.classList.add('radio-checked');
+    //     radioElement.parentElement.previousElementSibling.classList.remove('radio-checked');
+    //     radioElement.parentElement.nextElementSibling.classList.remove('radio-checked');
+    //   } else if (unfinishedRef.checked && todo.classList.contains('checked')) {
+    //     todo.classList.add('is-hidden');
+    //     radioElement.parentElement.classList.add('radio-checked');
+    //     radioElement.parentElement.previousElementSibling.classList.remove('radio-checked');
+    //     radioElement.parentElement.previousElementSibling.previousElementSibling.classList.remove('radio-checked');
+    //   }
+    // })
+
+    // if (e.target.id === 'finished') {
+    //   todoRefs.forEach(todo => {
+    //     if (!todo.classList.contains('checked')) {
+    //       todo.style.display = 'none';
+    //     }
+    //   });
+    // }
+  });
+})
+
+// console.log(refs.radioBtns)
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let todo = e.currentTarget[0].value;
+  
+  if (!todo) return;
+
+  renderTodo(todo);
+
+  e.currentTarget.reset()
+})
+
+function renderTodo(data) {
+  const todoEl = document.createElement('li');
+  todoEl.classList.add('todo');
+  todoEl.dataset.finished = 'false';
+  todoEl.innerHTML = `<button class="btn-delete is-hidden">
+                      <i class="far fa-minus-square">
+                      </i>
+                      </button>${data}`;
+  
+  const btn = todoEl.querySelector('.btn-delete');
+  btn.addEventListener('click', () => todoEl.remove());
+
+  toggleTodos(todoEl);
+
+  refs.todosUl.prepend(todoEl);
+}
+
+function toggleTodos(el) {
+  el.addEventListener('click', () => {
+    el.classList.toggle('checked');
+
+    if (el.classList.contains('checked')) {
+      el.dataset.finished = 'true';
+    } else {
+      el.dataset.finished = 'false';
+    }
+
+    el.children[0].classList.toggle('is-hidden');
+  });
+}
